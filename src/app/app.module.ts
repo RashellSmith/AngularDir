f674d2a56b34fbc4c8a8b36f07be5be9c4146552
Rashell <rashell.smith@zoho.com>
2019-08-27 13:21:24 -0400

    adding files to github repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompTwoComponent } from './comp-two/comp-two.component';
+import { CompThreeComponent } from './comp-three/comp-three.component';
+import { CompFourComponent } from './comp-four/comp-four.component';
+import { PersonFormComponent } from './person-form/person-form.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompTwoComponent,
+    CompThreeComponent,
+    CompFourComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
+  bootstrap: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
